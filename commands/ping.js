187d5600d9372f5b2f883a2158a532e11c2ecddc
KRRAN44 <7hg4vht7v4@privaterelay.appleid.com>
2026-08-24 00:48:56 -0600

    act

--- commands/ping.js
+++ commands/ping.js
@@ -1,10 +1,20 @@
-  module.exports = {
+   module.exports = {
     name: 'ping',
     aliases: ['p'],
 
-    async execute({ sock, jid }) {
+    async execute({ sock, jid, message }) {
+      const inicio = Date.now()
+
       await sock.sendMessage(jid, {
-        text: 'Pong 🏓'
+        text: '🏷 *Vanity-Bot* conectado correctamente'
+      })
+
+      const ping = Date.now() - inicio
+
+      await sock.sendMessage(jid, {
+        text: `\n⏱️ Tiempo de respuesta: ${ping} ms`
+      }, {
+        quoted: message
       })
     }
   }
